39ecfbe94f28f69e3328016c67a10570c460e0d3
祐介 富岡 <tomioka2012@gmail.com>
2020-05-04 23:23:56 +0900

    タオルがプレイヤーを識別するように修正

--- Assets/Scripts/Game/GimmickBehavior/SoftFloor.cs
+++ Assets/Scripts/Game/GimmickBehavior/SoftFloor.cs
@@ -8,6 +8,7 @@ using Zenject;
 public class SoftFloor : MonoBehaviour
 {
     public float reductionRatio = 0.1f;
+    public string playerLayer = "Player";
 
     private Crack playerCrack;
     private ObservableCollision2DTrigger targetCollisionTrigger;
@@ -23,11 +24,19 @@ public class SoftFloor : MonoBehaviour
     {
         this.targetCollisionTrigger
             .OnTriggerEnter2DAsObservable()
+            .Where(this.IsPlayer)
             .Subscribe(_ => this.PlayerEnter());
 
         this.targetCollisionTrigger
             .OnTriggerExit2DAsObservable()
+            .Where(this.IsPlayer)
             .Subscribe(_ => this.PlayerExit());
+    }
+
+    private bool IsPlayer(Collider2D collision)
+    {
+        var layerMask = LayerMask.GetMask(playerLayer);
+        return ((1 << collision.gameObject.layer) & layerMask) != 0;
     }
 
     private void PlayerEnter()
