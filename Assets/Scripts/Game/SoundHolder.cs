0b3f733ed6787e7cab8ad8be1ced83ac0761d0ca
祐介 富岡 <tomioka2012@gmail.com>
2020-05-05 11:51:04 +0900

    サウンドホルダーを追加

---
+++ Assets/Scripts/Game/SoundHolder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundHolder : MonoBehaviour
+{
+    public AudioSource bgm;
+    public AudioSource crack;
+    public AudioSource jump;
+    public AudioSource landing;
+}
